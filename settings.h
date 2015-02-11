57a4d08d0f7b07bd1af800ec757d7378200300fb
jessica-lee <jessica-lee@berkeley.edu>
2015-02-11 14:10:20 -0800

    Jessica's settings.

--- settings.h
+++ settings.h
@@ -7,14 +7,15 @@
 #ifndef __SETTINGS_H
 #define __SETTINGS_H
 
-#define IDENT_STR "BIOMIMETICS-ROACH;AMS-ENC;"
+#define IDENT_STR "JLEE-ROACH;AMS-ENC;"
 
 /////// Radio settings ///////
 /////// Radio settings ///////
-#define RADIO_CHANNEL       0x0F
-#define RADIO_PAN_ID        0x2000
-#define RADIO_SRC_ADDR      0x0001
+#define RADIO_CHANNEL       0x14
+#define RADIO_PAN_ID        0x2170
+#define RADIO_SRC_ADDR      0x2102
 
+//#define RADIO_DEST_ADDR       0x2101
 
 #define RADIO_TXPQ_MAX_SIZE   10
 #define RADIO_RXPQ_MAX_SIZE   10
@@ -25,7 +26,7 @@
 #define TELEMPACKFUNC(x) vrTelemGetData(x)
 
 // Encoders Setup
-#define AMS_ENC_OFFSET_0        5758
-#define AMS_ENC_OFFSET_1        7706
+#define AMS_ENC_OFFSET_0 -8052/4
+#define AMS_ENC_OFFSET_1 2580/4
 
 #endif //_SETTINGS_H
